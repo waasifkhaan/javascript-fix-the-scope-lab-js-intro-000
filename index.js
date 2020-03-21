de07910955f1e0c35229a408ae86bad4570327b4
Wasif Khan <waasif.khaan@gmail.com>
2020-03-21 22:36:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
   // Feel free to move things around!
   const two = 2
+  return n + two
+
 }
 
 var funkyFunction = function() {
